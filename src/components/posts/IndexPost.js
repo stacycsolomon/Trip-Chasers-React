4bf4d9cbca7598dadbb513d43e51a6fbdbe68a18
Stacy S <stacycsolomon@gmail.com>
2022-05-24 22:49:28 -0500

    Modified IndexPost component to use React Hooks

--- src/components/posts/IndexPost.js
+++ src/components/posts/IndexPost.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react'
+/* eslint-disable no-lone-blocks */
+/* eslint-disable no-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
+import React, { useState, useEffect } from 'react'
 import { indexPosts } from '../../api/post'
 import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button'
+import './posts.css'
 
-class IndexPost extends Component {
-  constructor (props) {
-    super(props)
+function IndexPost (props) {
+  const [posts, setPosts] = useState([])
 
-    this.state = {
-      posts: null
-    }
-  }
-
-  componentDidMount () {
-    const { user, msgAlert } = this.props
+  useEffect((posts) => {
+    const { user, msgAlert } = props
 
     indexPosts(user)
-      .then((res) => this.setState({ posts: res.data.posts }))
+      .then((res) => setPosts(res.data.posts))
       .then(() => {
         msgAlert({
           heading: 'Index Posts success',
@@ -30,33 +29,36 @@ class IndexPost extends Component {
           variant: 'danger'
         })
       })
-  }
+  }, [])
 
-  render () {
-    const { posts } = this.state
+  let postJSX
+  { posts
+    ? (postJSX = posts.map((post) => (
+      <div className='posts' key={post._id}>
+        <div className='profilePicture'>
+          <img
+            src={process.env.PUBLIC_URL + '/images/blank-profile-picture'}
+          />
+        </div>
+        <p>{post.description}</p>
+        <>
+          <Link to={`/posts/${post._id}`}>
+            <Button>Go to Post</Button>
+          </Link>
+        </>
+        <p>{post.img}</p>
+      </div>
+		  )))
+    : (postJSX = 'No posts, create some') }
 
-    if (posts === null) {
-      return 'Loading...'
-    }
-
-    let postJSX
-    if (posts.length === 0) {
-      postJSX = 'No posts, create some'
-    } else {
-      postJSX = posts.map((post) => (
-        <li key={post._id}>
-          <Link to={`/posts/${post._id}`}>{post.description}</Link>
-        </li>
-      ))
-    }
-
-    return (
-      <>
-        <h3>All posts:</h3>
-        <ul>{postJSX}</ul>
-      </>
-    )
-  }
+  return (
+    <>
+      <h3>All posts:</h3>
+      <div>
+        <ul style={{ display: 'flex', flexDirection: 'column-reverse' }}>{postJSX}</ul>
+      </div>
+    </>
+  )
 }
 
 export default IndexPost
